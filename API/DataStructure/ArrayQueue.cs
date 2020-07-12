7845b068d5b8b1efb883f22ee95f6fe5d960125a
fafagb <fafagb2009@aliyun.com>
2020-07-12 23:07:34 +0800

    add files

---
+++ API/DataStructure/ArrayQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.DataStructure
+{
+    //一次性队列
+    public class ArrayQueue<T>
+    {
+        // 队列数组
+        private readonly T[] _items;
+        private readonly int _size = 0;
+        private int _head = 0;
+        private int _tail = 0;
+
+        public ArrayQueue(int size) {
+            _items = new T[size];
+            _size = size;
+        }
+
+        public bool Enqueue(T item) {
+            if (_tail == _size) return false;
+            _items[_tail] = item;
+            _tail++;
+            return true;
+        }
+
+
+        public T Dequeue() {
+            if (_head == _tail) return default;
+            var ret = _items[_head];
+            _head++;
+            return ret;
+        }
+    }
+}
---
+++ API/DataStructure/ArrayQueue1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.DataStructure
+{
+    //对一次性队列进行优化，变成无限次使用队列
+    public class ArrayQueue1<T>
+    {
+        private readonly T[] _items;
+        private readonly int _size = 0;
+        private int _head = 0;
+        private int _tail = 0;
+
+        public ArrayQueue1(int size) {
+            _items = new T[size];
+            _size = size;
+        }
+
+        public bool Enqueue(T item) {
+            // 队列末尾没有空间了
+            if (_tail == _size) {
+                // 整个队列都满了
+                if (_head == 0) return false;
+
+                for (var i = _head; i < _tail; i++) {
+                    _items[i-_head] = _items[i];
+                }
+                // 搬移完以后重新更新head和tail
+                _tail -= _head;
+                _head = 0;
+            }
+
+            _items[_tail] = item;
+            _tail++;
+            return true;
+        }
+
+
+        public T Dequeue() {
+            if (_head == _tail) return default;
+            var ret = _items[_head];
+            _head++;
+            return ret;
+        }
+    }
+}
