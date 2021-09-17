f351ff2e184ab1ff08b3a377f561481c1e1bceff
fafagb <fafagb2009@aliyun.com>
2021-09-17 13:45:24 +0800

    update files

--- Tesla/ValidateArguments.cs
+++ Tesla/ValidateArguments.cs
@@ -1,56 +1,66 @@
 using System;
-using System.Collections.Generic; 
+using System.Linq;
+using System.Collections.Generic;
+namespace ArgsProblem.Tests
+{
+    public class ValidateArguments
+    {
+        public int Validate(string[] args)
+        {
+            int returnValue = -1;
 
-namespace   Tesla {
- public class ValidateArguments {
-        public int Validate (string[] args) {
-
-            int returnValue = 0;
-
+          if(args.Length==0) return -1;
         if(args.Length==0) return returnValue;
 if(args[0].ToLower()=="--help"&&args.Length<=2)
 {
  return 1;
 }
             Dictionary<string, string> dic = new Dictionary<string, string> ();
-            for (int i = 0; i < args.Length; i++) {
+            for (int i = 0; i < args.Length-1; i++) {
                 if (args[i].ToLower() == "--name") {
                     dic.Add ("--name", args[i + 1]);
                 }
-                if (args[i] == "--count") {
+                if (args[i].ToLower() == "--count") {
                     dic.Add ("--count", args[i + 1]);
                 }
-                if (args[i] == "--help") {
+                if (args[i].ToLower() == "--help") {
                     dic.Add ("--help", args[i + 1]);
                 }
             }
 
             if (dic.ContainsKey ("--name")) {
                 int itemLength = dic["--name"].Length;
-                if (!(itemLength >= 3 && itemLength <= 10)) {
+                if (itemLength >= 3 && itemLength <= 10) {
+                    returnValue = 0;
+                  
+                }else {
                     returnValue = -1;
-                    return returnValue;
                 }
             }
-            int result;
+          
 
             if (dic.ContainsKey ("--count")) {
+                  int result;
                 bool isInt = int.TryParse (dic["--count"], out result);
-                if (!(result >= 10 && result <= 100)) {
+                if (result >= 10 && result <= 100) {
+                    returnValue = 0;
+                 
+                }else {
                     returnValue = -1;
-                    return returnValue;
                 }
             }
-
-            if (dic.ContainsKey ("--help")) {
+if(returnValue == 0){
+if (dic.ContainsKey ("--help")) {
+                
                 returnValue =1 ;
             }
+                }
+            
 
             return returnValue;
 
             // Console.WriteLine("Sample debug output");
-            throw new System.ArgumentException ("Not yet implemented");
+           // throw new System.ArgumentException("Not yet implemented");
         }
     }
-
-    }
+}
